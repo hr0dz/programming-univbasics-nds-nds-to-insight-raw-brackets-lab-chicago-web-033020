42c8ad2b2bb0c64ec2cab33e7be647e3c9ee1334
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-30 04:11:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,20 +9,27 @@ result = {
  
 director_index = 0
 total = 0
-director = ""
 
 while director_index < nds.length do
   movie_index = 0
   director = nds[director_index][:name]
+  #this piece of code loops through every director name
+  #set variable director to equal each director
  
   while movie_index < nds[director_index][:movies].length do
-   # binding.pry
+    #this piece of code loops through each director's movies
+  
     total = total + nds[director_index][:movies][movie_index][:worldwide_gross]
      puts total
-  movie_index += 1
+     
+     #set variable to total to be 0, then add to itself all the way through each director's worldwide grosses
+     
+      movie_index += 1
+#binding.pry
+      end
+result[director] = total                                
 
-   end
-result[director] = total
+          #we add director var to our empty hash and set it equal to the total worldwide grosses
 total = 0
        director_index += 1
   end
